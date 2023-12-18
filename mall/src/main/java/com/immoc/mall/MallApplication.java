f56ca0ede5bb845789cc17c4a1b504d0cf9a78a0
lvjiuluan <17384775461@163.com>
2023-12-18 08:30:32 +0800

    台式机提交

--- mall/src/main/java/com/immoc/mall/MallApplication.java
+++ mall/src/main/java/com/immoc/mall/MallApplication.java
@@ -1,9 +1,11 @@
 package com.immoc.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.immoc.mall.dao")
 public class MallApplication {
 
     public static void main(String[] args) {
---
+++ mall/src/main/java/com/immoc/mall/dao/CategoryMapper.java
@@ -0,0 +1,10 @@
+package com.immoc.mall.dao;
+
+import com.immoc.mall.pojo.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+public interface CategoryMapper {
+    @Select("select * from mall_category where id = #{id}")
+    Category findById(Integer id);
+}
