ed0ef01124f424e72c47b81c60c9e5ccbf3b8b87
lvjiuluan <17384775461@163.com>
2023-12-18 17:22:59 +0800

    台式机提交支付项目第一次

---
+++ pay/src/main/java/com/immoc/pay/PayApplication.java
@@ -0,0 +1,13 @@
+package com.immoc.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+
+}
