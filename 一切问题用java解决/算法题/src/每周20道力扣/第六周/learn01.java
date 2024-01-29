f45a4ea1ac3b1adfbf66bff4d0586c4a45d9f859
lvjiuluan_global_level <lvjiuluan_global_level@163.com>
2024-01-29 10:44:30 +0800

    笔记本提交

--- 一切问题用java解决/算法题/src/每周20道力扣/第六周/learn01.java
+++ 一切问题用java解决/算法题/src/每周20道力扣/第六周/learn01.java
@@ -1,15 +1,14 @@
 package 每周20道力扣.第六周;
 
-import java.io.IOException;
 
-public class learn01 {
-    public static void hello(int n) throws IOException {
-        if (n < 0) {
-            throw new IOException();
-        }
-    }
+class A{
+    int a;
+}
+public class learn01 extends A{
+    int a;
 
-    public static void main(String[] args) throws IOException {
-        hello(0);
+    public static void main(String[] args) {
+        new learn01().a = 1;
     }
 }
+
