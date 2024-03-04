2104a1ae84bcefc8b770422e540f87a8cf69214d
lvjiuluan_global_level <lvjiuluan_global_level@163.com>
2024-03-04 11:39:50 +0800

    提交

--- 一切问题用java解决/平时编程问题/src/韩顺平Java/集合02/map_/Map01.java
+++ 一切问题用java解决/平时编程问题/src/韩顺平Java/集合02/map_/Map01.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Map01 {
     public static void main(String[] args) {
-        Map<String,String> map = new HashMap();
+        Map map = new HashMap();
         map.put("no1", "韩顺平");
         map.put("no2", "张无忌");
     }
