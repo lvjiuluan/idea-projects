79f6bc08bb1b3865524d5feb494e757391a0a3e6
lvjiuluan <17384775461@163.com>
2024-03-12 23:36:47 +0800

    4

---
+++ 一切问题用java解决/平时编程问题/src/韩顺平Java/Integer类/BaseMain.java
@@ -0,0 +1,14 @@
+package 韩顺平Java.Integer类;
+
+public class BaseMain {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+    }
+}
+
+class Base {
+
+}
+
+class sub extends Base {
+}
