508ce9559844748f066c6c14e84e0f2e502b580d
lvjiuluan_global_level <lvjiuluan_global_level@163.com>
2024-03-20 17:35:31 +0800

    1

---
+++ 牛客网论坛项目/p02/community02/src/main/java/com/nowcoder/community/controller/demo/HelloController.java
@@ -0,0 +1,10 @@
+package com.nowcoder.community.controller.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class HelloController {
+    
+}
---
+++ 牛客网论坛项目/p02/community02/src/test/java/com/nowcoder/community/CommunityApplicationTest.java
@@ -0,0 +1,15 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
