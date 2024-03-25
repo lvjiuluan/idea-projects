3bc660c901769a896c3da9f8daef9d7c3fcb368c
lvjiuluan <17384775461@163.com>
2024-03-25 08:15:11 +0800

    提交

---
+++ 牛客网论坛项目/p02/community02/src/main/java/com/nowcoder/community/interceptor/InterceptorA.java
@@ -0,0 +1,29 @@
+package com.nowcoder.community.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class InterceptorA implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("拦截器A preHandle执行了");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("拦截器A postHandle执行了");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("拦截器A afterCompletion执行了");
+    }
+}
