84b553f3a27317d274e26eb6324f963967f67a3e
lvjiuluan_global_level <lvjiuluan_global_level@163.com>
2024-04-19 11:31:38 +0800

    1

---
+++ 牛客网论坛项目/项目官方实现源码/mavendemo02/src/test/java/com/nowcoder/mavendemo02/TestExampleServlet.java
@@ -0,0 +1,14 @@
+package com.nowcoder.mavendemo02;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link com.nowcoder.mavendemo02.ExampleMacro}
+ */
+public class TestExampleServlet extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
