71adaaead7d46422515114c9185d9b4e9890fa67
lvjiuluan_global_level <lvjiuluan_global_level@163.com>
2024-04-26 08:57:30 +0800

    1

---
+++ 一切问题用java解决/平时编程问题/src/韩顺平Java/排序/TestComparator.java
@@ -0,0 +1,9 @@
+package 韩顺平Java.排序;
+
+import java.util.Comparator;
+
+public class TestComparator {
+    public static void main(String[] args) {
+        Comparator<Integer> reverseOrder = Comparator.reverseOrder();
+    }
+}
