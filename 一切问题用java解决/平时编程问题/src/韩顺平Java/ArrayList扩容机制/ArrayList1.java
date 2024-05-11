9f14355ed6779bb401fe1969ab65ceb722a8a6fb
lvjiuluan <17384775461@163.com>
2024-05-11 19:58:59 +0800

    1

---
+++ 一切问题用java解决/平时编程问题/src/韩顺平Java/ArrayList扩容机制/ArrayList1.java
@@ -0,0 +1,15 @@
+package 韩顺平Java.ArrayList扩容机制;
+
+import java.util.ArrayList;
+
+/*
+ *
+ *  无参构造函数
+ * */
+public class ArrayList1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        System.out.println(arrayList);
+    }
+}
---
+++ 一切问题用java解决/平时编程问题/src/韩顺平Java/ArrayList扩容机制/ArrayList2.java
@@ -0,0 +1,16 @@
+package 韩顺平Java.ArrayList扩容机制;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ArrayList2 {
+    public static void main(String[] args) {
+        int initialCapacity = 8;
+        ArrayList<Integer> arrayList = new ArrayList<>(initialCapacity);
+        for (int i = 0; i < 20; i++) {
+            arrayList.add(i);
+        }
+        System.out.println(arrayList);
+        HashMap
+    }
+}
