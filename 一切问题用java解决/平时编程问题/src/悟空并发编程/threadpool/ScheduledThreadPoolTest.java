dd21e65844af0d7873f8fb62204d5f22f1724427
lvjiuluan <17384775461@163.com>
2024-05-15 07:44:29 +0800

    1

---
+++ 一切问题用java解决/平时编程问题/src/悟空并发编程/threadpool/ScheduledThreadPoolTest.java
@@ -0,0 +1,13 @@
+package 悟空并发编程.threadpool;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledThreadPoolTest {
+    public static void main(String[] args) {
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(10);
+//        executorService.schedule(new Task(), 5, TimeUnit.SECONDS);
+        executorService.scheduleAtFixedRate(new Task(), 1, 3, TimeUnit.SECONDS);
+    }
+}
---
+++ 一切问题用java解决/平时编程问题/src/悟空并发编程/threadpool/ShuntDown.java
@@ -0,0 +1,43 @@
+package 悟空并发编程.threadpool;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/*
+ *   演示关闭线程池
+ *
+ * */
+public class ShuntDown {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 100; i++) {
+            executorService.execute(new ShuntDownTask());
+        }
+        Thread.sleep(1500);
+        List<Runnable> runnables = executorService.shutdownNow();
+        System.out.println(runnables.size());
+//        executorService.shutdown();
+//        System.out.println(executorService.awaitTermination(3, TimeUnit.SECONDS));
+
+//        System.out.println(executorService.isShutdown());
+//        System.out.println(executorService.isTerminated());
+//        executorService.shutdown();
+//        System.out.println(executorService.isShutdown());
+//        System.out.println(executorService.isTerminated());
+    }
+}
+
+class ShuntDownTask implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(500);
+            System.out.println(Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName() + "被中断了");
+        }
+    }
+}
